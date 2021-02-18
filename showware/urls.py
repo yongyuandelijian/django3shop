a86c1225ed654cd7df60a036be6fba70e4e4eef5
超级士兵6300 <yongyuandelijian@hotmail.com>
2021-02-18 09:09:49 +0800

    商品明细页面处理完毕

--- showware/urls.py
+++ showware/urls.py
@@ -1,8 +1,9 @@
 # showware的urls设置
 from django.urls import path
-from .views import waredetailView,showwareView
+from .views import waredetailView,showwareView,collectView
 
 urlpatterns=[
     path('.html',showwareView,name='showware'),
-    path('waredetail.<int:wareid>.html',waredetailView,name='waredetail')
+    path('waredetail.<int:wareid>.html',waredetailView,name='waredetail'),
+    path('collect.html',collectView,name='collect')
 ]
--- showware/views.py
+++ showware/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
 from .models import Ware_info,Ware_level
+from django.http import JsonResponse
+from django.db.models import F
 
 # Create your views here.
 # 展示商品  商品详细
@@ -37,12 +39,24 @@ def showwareView(request):
         pages=fenyeqi.page(fenyeqi.num_pages)
     return render(request,'warelist.html',locals())
 
-# urls里的路由设置了wareid的变量，这里视图方法也必须设置相同名称和类型的wareid否则会提示typeError
+# urls里的路由设置了wareid的变量，这里视图方法也必须设置相同名称和类型的wareid否则会提示typeError，如果路由有多个，那么视图函数也需要依次按照顺序设置多个，和路由参数完全匹配
 def waredetailView(request,wareid):
     title="炫酷的商品明细介绍"
     classContent="datails"
     wareInfo=Ware_info.objects.filter(id=wareid).first()
     recommend=Ware_info.objects.exclude(id=wareid).order_by('-sold')[:5]
-    likesList=request.session.get('likes',[])
+    likesList=request.session.get('likes',[]) # 好像还真的只有session里才记录了当前用户的收藏，问题是重启之后session里还有数据吗
     likes=True if wareid in likesList else False
     return render(request,"waredetail.html",locals())
+
+# ajax 请求处理视图
+def collectView(request):
+    id=request.GET.get('id','')   # 从get请求中将id拿出来，如果不存在则返回后面的设定值
+    result={"result":"已收藏"}     # 设置相应内容
+    likeList=request.session.get('likes',[])  # 从session中取出收藏，并且以列表的形式来展示，我总觉得这个地方应该设置一个关系表将他们存储起来
+    # 当传入的id为空或者不在当前用户的收藏列表中时候,F类可以将数据库查询对象的属性解析出来
+    if id and not int(id) in likeList:
+        Ware_info.objects.filter(id=id).update(likes=F('likes')+1)  # 更新属性，返回结果，并且更新session
+        result['result']="收藏成功"
+        request.session['likes']=likeList+[int(id)]  # 这样存储在session中是有问题的，一个是重启就会消失，并且用户非常多的时候对机器的内存要求也越来越高
+    return JsonResponse(result)   # 这个方法可以将python的字典转为json变成响应内容
