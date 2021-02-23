f0a103622aa187004c962857f0ae3e8b49562587
超级士兵6300 <yongyuandelijian@hotmail.com>
2021-02-24 06:42:16 +0800

    20210224

--- shopper/form.py
+++ shopper/form.py
@@ -3,6 +3,7 @@
 # 20210222
 from django import forms
 from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
 
 class LoginForm(forms.Form):
     '''创建登录表单'''
@@ -22,3 +23,37 @@ class LoginForm(forms.Form):
             return self.cleaned_data['username']
         else:
             raise ValidationError('用户名为手机号，输入不合法')
+
+class LoginModelForm(forms.ModelForm):
+    """使用ModelForm来创建登录表单字段，引用在html中即可"""
+    class Meta:
+        model=User
+        fields=('username','password')
+        labels={
+            'username':'请输入您的手机号',
+            'password':'请输入密码',
+        }
+        error_messages={
+            '__all__':{'required':'请输入内容','invalid':'请检查输入内容'}
+        }
+        # 定义widgets 设置表单字段对应html元素控件的属性
+        widgets={
+            'username':forms.widgets.TextInput(
+                attrs={
+                    'class':'layui-input','placeholder':'请输入手机号','lay-verify':'required|phone','id':'username'
+                }
+            ),
+            'password': forms.widgets.PasswordInput(
+                attrs={
+                    'class': 'layui-input', 'placeholder': '请输入密码', 'lay-verify': 'required|password', 'id': 'password'
+                }
+            )
+        }
+
+    # 自定义表单字段username的数据清洗
+    def clean_username(self):
+        if len(self.cleaned_data['username']) ==11:
+            return self.cleaned_data['username']
+        else:
+            raise ValidationError('用户名为手机号码')
+
--- shopper/views.py
+++ shopper/views.py
@@ -1,20 +1,44 @@
 from django.shortcuts import render,redirect,reverse    # 从快捷方式导入重定向后者直接提供我都忍了，这个reverse的功能居然想等于模板中的url 就是将路由命名解析成路由地址
 from django.contrib.auth.models import User     # 导入用户模型来查询数据
 from django.contrib.auth import login,authenticate  # 导入凭证的验证方法，以及登录方法
+from .form import *
+from .models import *
 from django.http import HttpResponse
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
-# from django.db import connection
-# cursor=connection.cursor()    # 利用连接创建游标
-# cursor.execute('select * from shoper_order_info where id=5')   # 执行sql
-# cursor.fetchone()   # 获取第一行数据
-# cursor.fetchall()   # 获取所有数据
+# 功能：登录，注销，购物车，个人中心
+# 装饰器用来校验有没有登录，如果没有登录则需要跳转到指定界面，这个装饰器三个参数
+# 1 function默认是None 定义装饰器执行的函数，一般不动
+# 2 通过redirect_field_name默认值next实现了登录之后就会自动跳转回来
+# 3 login_url 设置用户登录的url ,默认是setting.py中的配置属性LOGIN_URL 这个属性默认是没有的，需要自己增加
+@login_required(login_url='/shopper/login.html')
+def shopperView(request):
+    """展示当前用户基本信息"""
+    title="炫酷的个人中心"
+    classContent="informations"
+    pageNum=request.GET.get("pageNum",1)    # 订单信息的显示页数
+    # 如果订单已经支付，则先将会话中的订单信息存储到库里
+    buyTime=request.GET.get("buyTime",'')  # 系统订单支付时间
+    payTime=request.session.get('payTime','')  # 支付返回的时间
+    if buyTime and payTime and buyTime==payTime:
+        payInfo=request.session.get('payInfo','')
+        order_info.objects.create(**payInfo)
+        del request.session['payTime']
+        del request.session['payInfo']
 
+    # 根据当前用户查询订单信息
+    orderInfoList=order_info.objects.filter(user_id=request.user.id).order_by('-createtime')
+    # 分页功能,如果传入的页码不是数字，则显示第一页，如果传入的页码不存在，则显示最后一页,最后返回要给界面的当前页面元素
+    paginator=Paginator(orderInfoList,7)
+    try:
+        pages=paginator.page(pageNum)
+    except PageNotAnInteger:
+        pages=paginator.page(1)
+    except EmptyPage:
+        pages=paginator.page(paginator.num_pages)
 
-# 登录，注销，购物车，个人中心
-
-def shopperView():
-    return HttpResponse('一个炫酷的个人中心。。。')
+    return render(request,'shopper.html',locals())
 
 def loginView(request):
     title="正在进行炫酷的登录"
@@ -37,6 +61,42 @@ def loginView(request):
             # User.objects.create()   这个和上面create_user的区别是，create_user会自动给密码进行加密，而create会当成一般对象直接去创建存储到库里
             user.save()
     return render(request,'login.html',locals())
+
+def loginView2(request):
+    """使用loginForm类来实现登录功能"""
+    title="正在登录"
+    classContent="logins"
+    # 处理http的post请求
+    if request.method=="POST":
+        infos=LoginForm(data=request.POST)   # 感觉和上面原始的区别就是这句话，原始从request里获取数据的，现在从loginForm中获取的
+        # 验证表单数据是否准确
+        if infos.is_valid():
+            data=infos.cleaned_data
+            username=data['username']
+            password=data['password']
+            # 查找User是否已有用户信息
+            if User.objects.filter(username=username):
+                user=authenticate(username=username,password=password)
+                if user:
+                    login(request,user)
+                    return redirect(reverse("shopper:shopper"))
+                else:
+                    state="用户名或者密码不正确"
+            else:
+                state="注册成功"
+                d=dict(username=username,password=password,is_staff=1,is_active=1)
+                user=User.objects.create_user(**d)
+                user.save()
+        else:
+            # 获取到的信息验证不通过，以json格式输出
+            error_msg=infos.errors.as_json()
+            print(error_msg)
+    else:
+        # 处理get请求
+        infos=LoginForm()
+        return render(request,"login.html",locals())
+
+
 
 
 def loginoutView():
